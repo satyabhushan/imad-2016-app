186b2091e695003319b8ee631fd09f03470086e7
satyabhushan <satya73989@gmail.com>
2016-11-20 15:11:26 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-
+app.get('/ui/imad2.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'imad2.html'));
+});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
