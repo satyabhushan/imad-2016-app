ed44c5296b88ba142678cdd6f53886c7ae5b4f89
satyabhushan <satya73989@gmail.com>
2016-11-26 23:06:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ app.get('/topic/:id',function(req,res){
 
 app.get('/887/:id',function(req,res){
     //if(req.session && req.session.auth && req.session.auth.user){
-        pool.query("SELECT articles.artid FROM articles INNER JOIN tagscon ON articles.artid = tagscon.artid",function(err,result){
+        pool.query('SELECT articles.artid FROM articles INNER JOIN tagscon ON articles.artid = tagscon.artid',function(err,result){
             //if(err){
               //  res.send('c working');
             //}else{
