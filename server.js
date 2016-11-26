da482ca54fdb59f22c9c15e36ce508ab5beb0f24
satyabhushan <satya73989@gmail.com>
2016-11-26 19:43:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,13 +83,13 @@ app.get('/topic/:id',function(req,res){
 });
 
 app.get('/887/:id',function(req,res){
-    if(req.session && req.session.auth && req.session.auth.user){
-        //pool.query("SELECT  from articles a left join tagscon b on a.artid==b.artid limit 10",function(err,result){
-          //  res.send(JSON.stringify(result.rows));
-        //});
-    }else {
+    //if(req.session && req.session.auth && req.session.auth.user){
+        pool.query("SELECT  from articles a left join tagscon b on a.artid==b.artid limit 10",function(err,result){
+            res.send(JSON.stringify(result.rows));
+        });
+    //}else {
          res.send('not working');
-    }
+    //}
     
 });
 
