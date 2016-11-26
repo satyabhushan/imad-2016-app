37e2202a1995f56a084a92b5ee4bd9e29ec20c08
satyabhushan <satya73989@gmail.com>
2016-11-26 20:51:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ app.get('/topic/:id',function(req,res){
 
 app.get('/887/:id',function(req,res){
     //if(req.session && req.session.auth && req.session.auth.user){
-        pool.query("SELECT a.artid from articles as a",function(err,result){
+        pool.query("SELECT a.artid from (articles as a)",function(err,result){
             if(err){
                 res.send('c working');
             }else{
