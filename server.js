ee51bdc3e610e08caa7ce9c56831b5adc2d1a444
satyabhushan <satya73989@gmail.com>
2016-12-04 23:56:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,10 +1,2 @@
-/*var express = require('express');
-var morgan = require('morgan');
-var path = require('path');*/
-var express = require('express');
-var morgan = require('morgan');
-var path = require('path');
-var crypto = require('crypto');
-//var session=require('express-session');
 
 console.log('hrllo');
