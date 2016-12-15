0b066ac6e89ace8e6b1902481abfaa7c44768aec
satyabhushan <satya73989@gmail.com>
2016-12-15 15:21:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -26,31 +26,12 @@ var config = {
 
 var pool = new Pool(config);
 
-app.post('/hash',function(req,res){
-    //var tc=req.params.input;
-    //var tc2=hash(tc,'random-string');
-    //delete req.session.auth;
-    req.session.auth = {'user' : 'awesome'};
-    res.send('logged in');
-});
-
-app.get('/new',function(req,res){
-    if(req.session && req.session.auth && req.session.auth.user){
-        res.send('working');
-    }else {
-         res.send('not working');
-    }
-});
 
 function hash(inputstring,salt)
 {
     var hashed=crypto.pbkdf2Sync(inputstring,salt, 100000, 512, 'sha512');
     return ["pbkdf2","10000",salt,hashed.toString('hex')].join('$');
 }
-
-/*app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});*/
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
@@ -72,50 +53,71 @@ app.get('/topic/:id',function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'imad2.html'));
 });
 
-function isloggedin(req){
+function islogin(req){
     if(req.session && req.session.auth && req.session.auth.user){
         return true;
-    }else {
+    }else{
         return false;
     }
 }
 
-app.get('/887/:id',function(req,res){
-    //if(req.session && req.session.auth && req.session.auth.user){
-        pool.query('SELECT a.artid,a.arttit,a.artdes,a.arttime,a.artuserid,c.tagid,c.tagname FROM articles a left JOIN tagscon b ON a.artid = b.tagartid left JOIN tags c on b.tagid=c.tagid',function(err,result){
+function isloggedin(req,res,otherdata){
+    var meta = {},data = {};
+    if(req.session && req.session.auth && req.session.auth.user){
+        console.log('1')
+        pool.query("SELECT * from users where userid=$1",[req.session.auth.user],function(err,result){
             if(err){
-                res.status(500).send(err.toString());
+                console.log('2')
+                meta.loggedin = false;
             }else{
-                var data=[],check=[],ind=-1,nol,noc;
-                for(var i=0,l=result.rows.length;i<l;i++){
-                    ind = check.indexOf(result.rows[i].artid);
-                    if(ind>-1){
-                        data[ind].tags.push({tagid:result.rows[i].tagid,tagname:result.rows[i].tagname});
-                    }else{
-                        /*pool.query("SELECT sum(1) as noc from likes a where="+result.rows[i].artid,function(err,result){
-                            nol = result.rows[0].noc || 0;
-                            pool.query("SELECT sum(1) as nol from comments a where="+result.rows[i].artid,function(err,result){
-                                noc = result.rows[0].nol || 0;
-                                check.push(result.rows[i].artid);
-                                data.push({artid:result.rows[i].artid,arttit:result.rows[i].arttit,artdes:result.rows[i].artdes,arttime:result.rows[i].arttime,artuserid:result.rows[i].artuserid,noc:noc,nol:nol,tags:[{tagid:result.rows[i].tagid,tagname:result.rows[i].tagname}]});
-                                
-                            });
-                        });*/
-                        check.push(result.rows[i].artid);
-                        data.push({artid:result.rows[i].artid,arttit:result.rows[i].arttit,artdes:result.rows[i].artdes,arttime:result.rows[i].arttime,artuserid:result.rows[i].artuserid,tags:[{tagid:result.rows[i].tagid,tagname:result.rows[i].tagname}]});
-                    }
+                if(result.rows.length === 1){
+                    console.log('3')
+                    meta.loggedin = true;
+                    meta.userInfo = {id: result.rows[0].userid, webid:result.rows[0].userfrom, image:result.rows[0].userpic,email:result.rows[0].email,name:result.rows[0].username,uswebid:result.rows[0].userwebid };
+                }else{
+                    console.log('4')
+                    meta.loggedin = false;
                 }
-                var ran = Math.floor(Math.random()*13);
-                pool.query("SELECT tagid,tagname,tagimg from tags where tagid="+(ran%5+1)+" or tagid="+(ran%5+2)+" or tagid="+(ran%5+3)+" or tagid="+(ran%5+4)+" or tagid="+(ran%5+5)+"",function(err,result){
-                    data = { arts: data,kftags: result.rows };
-                    data = { isloggedin : isloggedin(req), otherdata: data  };
-                    res.send(JSON.stringify(data));                    
-                });
             }
+            data = {meta: meta, otherdata:otherdata};
+            res.send(JSON.stringify(data));
         });
-    //}else {
-         //res.send('not working');
-    //}
+    }else {
+        console.log('5')
+        meta.loggedin = false;
+        data = {meta: meta, otherdata:otherdata};
+        res.send(JSON.stringify(data));
+    }
+}
+
+app.get('/887/:id',function(req,res){
+    pool.query("select a.artid,a.arttit,a.arttit,a.artdes,d.tagid,d.tagimg,d.tagname from articles a inner join tagscon b on a.artid = b.tagartid "+ (islogin(req) ? " inner join usrflwngtg c on b.tagid  = c.tagid and c.userid = 14  left join tags d on d.tagid=b.tagid" : " left join tags d on d.tagid=b.tagid where b.tagid = 1 or b.tagid = 2 or b.tagid = 8 or b.tagid = 6 ")+" ORDER by random()",function(err,result){
+        if(err){
+            console.log(err);
+            res.status(500).send(err.toString());
+        }else{
+            var data=[],check=[],ind=-1,nol,noc;
+            for(var i=0,l=result.rows.length;i<l;i++){
+                ind = check.indexOf(result.rows[i].artid);
+                if(ind>-1){
+                    data[ind].tags.push({tagid:result.rows[i].tagid,tagname:result.rows[i].tagname});
+                }else{
+                    check.push(result.rows[i].artid);
+                    data.push({artid:result.rows[i].artid,arttit:result.rows[i].arttit,artdes:result.rows[i].artdes,arttime:result.rows[i].arttime,artuserid:result.rows[i].artuserid,tags:[{tagid:result.rows[i].tagid,tagname:result.rows[i].tagname}]});
+                }
+            }
+
+            pool.query("select * from tags t "+ (islogin(req) ? "where not exists (select 1 from usrflwngtg Where t.tagid=tagid and userid="+req.session.auth.user+")" : "")+ " ORDER BY RANDOM() limit 5",function(err,result){
+                if(err){
+                    console.log(err);
+                    res.status(500).send(err.toString());
+                }else{
+                    data = { arts: data,kftags: result.rows };
+                    isloggedin(req,res,data);
+                }               
+            });
+        }
+    });
     
 });
 
